73246875465d7e5e37beb2ad3ec8ec63c34e20dc
mestepa1991 <miguelalonsoestepa@gmail.com>
2026-01-29 23:47:11 -0500

    metodo de carge perfiles

--- src/Controllers/Admin/PermisoController.php
+++ src/Controllers/Admin/PermisoController.php
@@ -55,7 +55,7 @@ class PermisoController extends GenericController {
             $this->model->clearPermisos($id);
             foreach ($input as $permiso) {
                 // Sincronizado con nombres de columnas reales
-                $this->model->insert([
+                $this->model->create([
                     'id_perfil'    => $id,
                     'id_modulo'    => $permiso['id_modulo'],
                     'can_ver'      => $permiso['ver'] ?? 0,
@@ -74,5 +74,42 @@ class PermisoController extends GenericController {
     // Firma corregida para eliminar error P1038 de Intelephense
     public function update($id, $input = null) {
         return $this->savePermisos($id, $input);
+    }
+   /**
+     * @OA\Get(
+     * path="/perfiles/permisos/{id}/check-all",
+     * operationId="getAllAccess",
+     * tags={"Seguridad - Permisos"},
+     * summary="Obtener todos los permisos formateados para validación inicial",
+     * @OA\Parameter(name="id", in="path", description="ID del Perfil", required=true, @OA\Schema(type="integer")),
+     * @OA\Response(response=200, description="Matriz de permisos optimizada")
+     * )
+     */
+    public function getAllAccess($id) {
+        if (!$id) {
+            http_response_code(400);
+            return json_encode(['error' => 'ID de perfil requerido']);
+        }
+
+        $data = $this->model->getMatrix($id);
+        $permissionsMap = [];
+
+        foreach ($data as $p) {
+            $item = (array)$p;
+            $idModulo = $item['id_modulo'];
+
+            // Creamos un mapa indexado por ID de módulo
+            $permissionsMap[$idModulo] = [
+                'ver'      => (isset($item['can_ver']) && $item['can_ver'] == 1) || (isset($item['ver']) && $item['ver'] == 1),
+                'crear'    => (isset($item['can_crear']) && $item['can_crear'] == 1) || (isset($item['crear']) && $item['crear'] == 1),
+                'editar'   => (isset($item['can_editar']) && $item['can_editar'] == 1) || (isset($item['editar']) && $item['editar'] == 1),
+                'eliminar' => (isset($item['can_eliminar']) && $item['can_eliminar'] == 1) || (isset($item['eliminar']) && $item['eliminar'] == 1)
+            ];
+        }
+
+        return json_encode([
+            'perfil_id' => (int)$id,
+            'modulos'   => $permissionsMap
+        ]);
     }
 }
